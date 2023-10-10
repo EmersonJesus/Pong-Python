175f7a308c48e341b10fa0b1ffc9c494294b6e96
Emerson Jesus <Emersoncruz73@hotmail.com>
2023-10-10 12:24:30 -0300

    v.01

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ import pygame as pg
 from pygame.locals import *
 import sys
 
+# Classe bola
+class Bola:
+    def __init__(self, tela, cor, posX, posY, raio):
+        self.tela = tela
+        self.cor = cor 
+        self.posX = posX
+        self.posY = posY
+        self.raio = raio
+        self.desenha_bola()
+    
+    def desenha_bola(self):
+        pg.draw.circle(self.tela, self.cor, (self.posX, self.posY), self.raio)
+        
+
 pg.init()
 
 # Cores RGB
@@ -13,8 +27,16 @@ largura = 900
 altura = 500
 tamanho_tela = (largura, altura)
 tela = pg.display.set_mode(tamanho_tela)
-tela.fill(preto)
 pg.display.set_caption('PONG')
+
+def quadra():
+    tela.fill(preto)
+    pg.draw.line(tela, branco, (largura//2, 0), (largura//2, altura), 5)
+
+quadra()
+
+# Criando os objetos
+bola = Bola(tela, branco, largura//2, altura//2, 15)
 
 # Loop do jogo
 while True:
